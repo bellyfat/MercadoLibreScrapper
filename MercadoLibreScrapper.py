1d0d004fa89a7b205d0e87e3d76c9e2f712c25f5
HerGuz27 <69280054+HerGuz27@users.noreply.github.com>
2021-01-27 17:10:06 -0300

    Add files via upload

--- MercadoLibreScrapper.py
+++ MercadoLibreScrapper.py
@@ -2,26 +2,54 @@ from time import sleep
 from datetime import date
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import requests
+import pandas as pd
 browser = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')
-browser.get('https://autos.mercadolibre.com.uy')
-sleep(8)
-count=0
-for ol in range(1,17):
+datos=[]
+
+def Scrapping(url):
+    browser.get(url)
+    sleep(8)
+    for ol in range(1,17):
      for li in range(1,4):
-      parseol=str(ol)
-      parseli=str(li)
-      url_img=('https://http2.mlstatic.com/D_NQ_NP_839600-MLU44669200907_012021-W.webp')
-      #url_img=browser.find_element_by_xpath('//*[@id="root-app"]/div/div/section/ol['+parseol+']/li['+parseli+']/div/div/div[1]/a/div/div/div/div/div[1]/img').get_attribute('src')
-      def foto(name_img):
-       name_img=name_img
-       img=requests.get(url_img).content
-       with open(name_img,'wb') as handler:
-        handler.write(img)
-       sleep(5) 
-      count=count+1
-      strcount=str(count)
-      name_img=strcount+".jpg"
-      foto(name_img)
+         parseol=str(ol)
+         parseli=str(li)
+         url=browser.find_element_by_xpath('//*[@id="root-app"]/div/div/section//ol['+parseol+']/li['+parseli+']/div/div/div[1]/a').get_attribute('href')
+         title=browser.find_elements_by_xpath('//*[@id="root-app"]/div/div/section/ol['+parseol+']/li['+parseli+']/div/div/a/div/div[3]/h2')[0].text
+         precio=browser.find_elements_by_xpath('//*[@id="root-app"]/div/div/section/ol['+parseol+']/li['+parseli+']/div/div/a/div/div[1]/div/div/span/span[2]')[0].text
+         km=browser.find_elements_by_xpath('//*[@id="root-app"]/div/div/section/ol['+parseol+']/li['+parseli+']/div/div/a/div/div[2]/ul/li[2]')[0].text
+         año=browser.find_elements_by_xpath('//*[@id="root-app"]/div/div/section/ol['+parseol+']/li['+parseli+']/div/div/a/div/div[2]/ul/li[1]')[0].text
+         ubicacion=browser.find_elements_by_xpath('//*[@id="root-app"]/div/div/section/ol['+parseol+']/li['+parseli+']/div/div/a/div/div[4]/span')[0].text
+         cars_data={'Auto':title,'precio':precio,'km':km,'año':año,'ubicacion':ubicacion,'url':url}
+        
+         def savedata(datos):
+            string="{},{},{},{},{},{}".format(
+                cars_data['Auto'],
+                cars_data['precio'],
+                cars_data['km'],
+                cars_data['año'],
+                cars_data['ubicacion'],
+                cars_data['url'],
+                )
+            save(string,'autos.csv')    
+         price=precio.replace('.','')
+         priceint=int(price)
+         if(priceint<5000):
+          savedata(datos)
+    return datos
+num=1
+
+def save(text,csv):
+    print(text)
+    with open(csv,'a') as fd:
+        fd.write(text+'\n')
+
+
+
+while True:
+ parseo=str(num)
+ url='https://autos.mercadolibre.com.uy/_Desde_'+parseo
+ Scrapping(url)
+ num=num+48
+ sleep(10)
 
 browser.quit()
